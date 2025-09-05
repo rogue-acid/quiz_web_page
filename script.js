a6f30b3460f2ecc40c981b92cbb25da1d23fdfe5
rogue-acid <galyakrinsky@gmail.com>
2025-09-05 13:48:10 -0700

    fix: fix question element not being centered

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 	$(document).ready(function() {
 		
+	let score = 0;
+	
+	
 	$(".anwser").click(function() {
 		console.log("anwser");
 		$(this).css("background-color", "lightgreen");
+		score += 1;
+		console.log(score);
 	});
 	
 	$(".wrong_anwser").click(function() {
 		console.log("wrong");
 		$(this).css("background-color", "tomato");
+		score -= 1;
+		console.log(score);
 	});
 
-	$(".next_button").click(function() {
+	$("#next_button").click(function() {
 		console.log("next questions");
 	});
 	
-	$("p").hover(function(){
-		$(this).css("background-color", "yellow");
-	}, function(){
-		$(this).css("background-color", "white");
-	});
-
 });

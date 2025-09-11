f36944def67aa79add9c73aab2c8b7d140c2b5ba
rogue-acid <example@example.com>
2025-09-11 01:00:11 -0700

    feat: add more questions, add score system, add end quiz page and restart

--- script.js
+++ script.js
@@ -1,20 +1,30 @@
-	$(document).ready(function() {
-		
+$(document).ready(function() {
 	let score = 0;
-	
-	
-	$(".anwser").click(function() {
-		console.log("anwser");
-		$(this).css("background-color", "lightgreen");
-		score += 1;
-		console.log(score);
+	let clicked = false;
+	score = +(sessionStorage.score)
+	$("#score").text("score: " + score + "/8");
+		
+	$("#start").click(function() {
+		console.log("test");
+		sessionStorage.score = 0;
 	});
 	
-	$(".wrong_anwser").click(function() {
+	$(".answer").click(function() {
+		console.log("answer");
+		$(this).css("background-color", "lightgreen");
+		
+		if (clicked == false) {
+			score += 1;
+			sessionStorage.score = score;
+			$("#score").text("score: " + score + "/8");
+			console.log(score);
+			clicked = true;
+		}
+	});
+	
+	$(".wrong_answer").click(function() {
 		console.log("wrong");
 		$(this).css("background-color", "tomato");
-		score -= 1;
-		console.log(score);
 	});
 
 	$("#next_button").click(function() {
